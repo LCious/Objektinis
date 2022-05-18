931e84be8ba0730da9f0b59600632010998c76d0
LCious <103113016+LCious@users.noreply.github.com>
2022-05-18 05:38:56 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,55 +1,80 @@
-#include <iostream>
-#include <iomanip>
-#include <vector>
-#include <algorithm>
-#include <time.h>
-#include <stdlib.h>
-#include <fstream>
 #include "functions.hpp"
-
 using namespace std;
 
 int main(){
 	srand(time(NULL));
-	int studNum = 0; char rt = '0';		// rt naudojamas vidurkio isvedimui pasirinkti (resultType)
+	char rt = '0';		// rt naudojamas vidurkio isvedimui pasirinkti (resultType)
 	bool vid;
-	Studentas* studentas = new Studentas[studNum];
-	try{
+
+	vector <Studentas> studentas;
+	vector <Studentas> winner;
+	vector <Studentas> loser;
+
+	cout << "Ar norite atlikti programos veikimo (greicio) analize? (Y/N): ";
+	if(Checker()){
+		cout << endl << "Ar norite apskaiciuoti mediana(1), vidurki(2)? ";
+		choiceCheck(rt);																		// 		GREICIO ANALIZE \/
+		cout << endl << "Failas kursiokai1000.txt" << endl;
+		generateStud(1000, "kursiokai1000.txt");
+		Ivestis(studentas, "kursiokai1000.txt");
+		calcFinal(studentas, rt);
+		group(studentas, winner, loser);
+		sort(studentas.begin(), studentas.end(), studComp);
+		fileOutput(winner, loser, rt, "kursiokai1000.txt");
+
+		cout << endl << "Failas kursiokai10000.txt" << endl;
+		generateStud(10000, "kursiokai10000.txt");
+		Ivestis(studentas, "kursiokai10000.txt");
+		calcFinal(studentas, rt);
+		group(studentas, winner, loser);
+		sort(studentas.begin(), studentas.end(), studComp);
+		fileOutput(winner, loser, rt, "kursiokai10000.txt");
+
+		cout << endl << "Failas kursiokai100000.txt" << endl;
+		generateStud(100000, "kursiokai100000.txt");
+		Ivestis(studentas, "kursiokai100000.txt");
+		calcFinal(studentas, rt);
+		group(studentas, winner, loser);
+		sort(studentas.begin(), studentas.end(), studComp);
+		fileOutput(winner, loser, rt, "kursiokai100000.txt");
+
+		cout << endl << "Failas kursiokai1000000.txt" << endl;
+		generateStud(1000000, "kursiokai1000000.txt");
+		Ivestis(studentas, "kursiokai1000000.txt");
+		calcFinal(studentas, rt);
+		group(studentas, winner, loser);
+		sort(studentas.begin(), studentas.end(), studComp);
+		fileOutput(winner, loser, rt, "kursiokai1000000.txt");
+
+		cout << endl << "Failas kursiokai10000000.txt" << endl;
+		generateStud(10000000, "kursiokai10000000.txt");
+		Ivestis(studentas, "kursiokai10000000.txt");
+		calcFinal(studentas, rt);
+		group(studentas, winner, loser);
+		sort(studentas.begin(), studentas.end(), studComp);
+		fileOutput(winner, loser, rt, "kursiokai10000000.txt");
+	}else{						// iprasta programos eiga \/
 		if(FileExist()){
-		cout << "Ka norite daryti? Skaityti duomenis is failo ar duomenis ivesti? (y - file / n - manual): ";
-		if(Checker()){
-			Studentas stud;
-			Ivestis(stud, studentas, studNum);
-		}
-		else{
+			cout << "Ar norite skaityti duomenis is failo ar duomenis ivesti rankiniu budu? (y - file / n - manually): ";
+			if(Checker()){
+				Ivestis(studentas, "kursiokai.txt");
+			}else{
+				do{
+					NewStudent(studentas);
+					cout << endl << "Ar norite prideti dar viena studenta? (Y/N) ";
+				} while(Checker());
+			}
+		}else {			// only manual input
+			cout << "Failas nerastas. Duomenys privalo buti ivesti rankiniu budu." << endl << endl;
 			do{
-				Studentas stud;
-				NewStudent(stud);
-				processStudent(stud, studentas, studNum);
-				cout << endl << "Ar norite prideti studenta? (Y/N) ";
+				NewStudent(studentas);
+				cout << endl << "Ar norite prideti dar viena studenta? (Y/N) ";
 			} while(Checker());
 		}
+		sort(studentas.begin(), studentas.end(), studComp);
+		cout << endl << "Ar norite apskaiciuoti mediana(1), vidurki(2), ar abu(3)? ";
+		choiceCheck(rt);
+		isvestis(studentas, rt);
 	}
-	else {			// Manual input
-		throw 1;
-		do{
-			Studentas stud;
-			NewStudent(stud);
-			processStudent(stud, studentas, studNum);
-			cout << endl << "Ar norite prideti studenta? (Y/N) ";
-		} while(Checker());
-	}
-	}catch(int e){
-		switch(e){
-			case 1:
-			cout << "Failas nerastas. Prasome duomenis ivesti rankiniu budu" << endl << endl;
-			break;
-		}
-		exit(1);
-	}
-	
-	sort(studentas, studentas + studNum, studComp);				//rusiavimas pagal varda
-	cout << endl << "Ka noretumete apskaiciuoti mediana(1), vidurki(2), ar abu(3)? ";
-	choiceCheck(rt);
-	isvedimas(studentas, rt, studNum);
+	system("PAUSE");
 }
